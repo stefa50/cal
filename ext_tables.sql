c3c95b6f01ffb29efc0176e9f354043c24f2e34a
geraldaistleitner <gerald.aistleitner@gmail.com>
2019-06-12 12:30:22 +0200

    Update ext_tables.sql
    ext_url is not required in TCA

--- ext_tables.sql
+++ ext_tables.sql
@@ -47,7 +47,7 @@ CREATE TABLE tx_cal_event (
 	shared_user_cnt int(11) unsigned DEFAULT '0' NOT NULL,
 	type tinyint(4) DEFAULT '0' NOT NULL,
 	page int(11) DEFAULT '0' NOT NULL,
-	ext_url tinytext NOT NULL,
+	ext_url tinytext,
 	isTemp tinyint(1) DEFAULT '0' NOT NULL,
 	icsUid text,
 	image tinyblob NOT NULL,
